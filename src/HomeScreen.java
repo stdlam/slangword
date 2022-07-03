85774dd5a0a838a0fba0a9188ed2cd35509cf57c
stdlam <stdiodl@gmail.com>
2022-07-04 00:16:42 +0700

    done 5 features
    done 1 2 3 7 8 functions

--- src/HomeScreen.java
+++ src/HomeScreen.java
@@ -1,32 +1,44 @@
+import java.awt.ComponentOrientation;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.logging.Handler;
 
 import javax.swing.JFrame;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.JList;
+import javax.swing.ListSelectionModel;
 
 public class HomeScreen {
 
 	private JFrame frame;
 	private JTextField textFieldSearch;
 	private Map<String, Slang> baseSlangs = new HashMap<String, Slang>();
-	private static String FILE_PATH = "src/slang.txt";
-	DefaultListModel<String> listMode = new DefaultListModel<String>();
+	
+	private static String SLANGS_FILE_PATH = "src/slang.txt";
+	private static String HISTORIES_FILE_PATH = "src/histories.txt";
+	
+	private DefaultListModel<String> listMode = new DefaultListModel<String>();
+	private DefaultListModel<String> historiesMode = new DefaultListModel<String>();
+	private JButton btnReset = new JButton("Reset");
+	private JLabel lblHistories = new JLabel("");
 
 	/**
 	 * Launch the application.
@@ -53,7 +65,7 @@ public class HomeScreen {
 	}
 	
 	private void setupData() {
-		baseSlangs = readData(FILE_PATH);
+		baseSlangs = readSlangsData(SLANGS_FILE_PATH);
 	}
 
 	/**
@@ -61,7 +73,7 @@ public class HomeScreen {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 666, 525);
+		frame.setBounds(100, 100, 666, 580);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -70,59 +82,152 @@ public class HomeScreen {
 		frame.getContentPane().add(lblWord);
 		
 		textFieldSearch = new JTextField();
-		textFieldSearch.setBounds(32, 98, 249, 26);
+		textFieldSearch.setBounds(34, 131, 244, 36);
 		frame.getContentPane().add(textFieldSearch);
 		textFieldSearch.setColumns(10);
 		
 		JButton btnSearch = new JButton("Search");
-		btnSearch.setBounds(284, 98, 117, 29);
+		btnSearch.setBounds(278, 136, 117, 29);
+		btnSearch.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				startSearch();
+			}
+			
+		});
 		frame.getContentPane().add(btnSearch);
 		
-		JList listHistories = new JList();
-		listHistories.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-		listHistories.setBounds(34, 60, 609, 26);
-		frame.getContentPane().add(listHistories);
+		lblHistories.setText(readHistoriesData());
+		JScrollPane historiesScrollPane = new JScrollPane();
+		historiesScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		historiesScrollPane.setBounds(34, 60, 609, 59);
+		historiesScrollPane.setViewportView(lblHistories);
+		frame.getContentPane().add(historiesScrollPane);
+		
+		historiesScrollPane.setViewportView(lblHistories);
 		
 		JButton btnAdd = new JButton("New Word");
 		btnAdd.setBounds(526, 27, 117, 29);
 		frame.getContentPane().add(btnAdd);
 		
-		JButton btnReset = new JButton("Reset");
 		btnReset.setBounds(413, 27, 117, 29);
+		btnReset.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				resetResultList();
+			}
+			
+		});
 		frame.getContentPane().add(btnReset);
 		
 		JButton btnDelete = new JButton("Delete");
 		btnDelete.setEnabled(false);
-		btnDelete.setBounds(575, 99, 68, 26);
+		btnDelete.setBounds(575, 137, 68, 26);
 		frame.getContentPane().add(btnDelete);
 		
 		JButton btnEdit = new JButton("Edit");
 		btnEdit.setEnabled(false);
-		btnEdit.setBounds(509, 98, 68, 29);
+		btnEdit.setBounds(508, 136, 68, 29);
 		frame.getContentPane().add(btnEdit);
 		
 		JList<String> listResult = new JList<String>(listMode);
 		listResult.setVisibleRowCount(30);
 		JScrollPane scrollPane = new JScrollPane(listResult);
-		scrollPane.setBounds(34, 136, 609, 349);
+		scrollPane.setBounds(34, 188, 609, 349);
 		frame.getContentPane().add(scrollPane);
 		
-		setupViews(listResult, lblWord);
+		setupViews(lblWord);
 	}
 	
-	private void setupViews(JList<String> list, JLabel lblWord) {
-		Object[] keys = baseSlangs.keySet().toArray(); 
-		for (int i = 0; i < keys.length; i++) {
-			Slang slang = baseSlangs.get(keys[i]);
-			listMode.add(i, slang.toString());
+	private void startSearch() {
+		if (!textFieldSearch.getText().isEmpty()) {
+			String keyword = textFieldSearch.getText();
+			Object[] keys = { keyword };
+			setupResultList(keys, true);
+			historiesMode.addElement(keyword);
+			if (lblHistories.getText().isEmpty()) {
+				lblHistories.setText(keyword);
+			} else {
+				lblHistories.setText(lblHistories.getText() + ", " + keyword);
+			}
+			writeData(HISTORIES_FILE_PATH, lblHistories.getText(), false);
 		}
+	}
+	
+	private void searchForMeaning(String keyword) {
+		btnReset.setEnabled(false);
+		listMode.removeAllElements();
+		int index = 0;
+		Object[] data = baseSlangs.values().toArray();
+		for (int i = 0; i < data.length; i++) {
+			Slang s = (Slang) data[i];
+			if (s.getMeaning().contains(keyword)) {
+				listMode.add(index++, data[i].toString());
+			}
+		}
+	}
+	
+	private void showDialog(String title, String content) {
+		JDialog d1 = new JDialog();
+		 
+        // create a label
+        JLabel l = new JLabel(content);
+
+        d1.getContentPane().add(l);
+
+        // setsize of dialog
+        d1.setSize(200, 200);
+
+        // set location of dialog
+        d1.setLocation(200, 200);
+
+        // set visibility of dialog
+        d1.setVisible(true);
+	}
+	
+	private void resetResultList() {
+		Object[] keys = baseSlangs.keySet().toArray(); 
+		setupResultList(keys, false);
+	}
+	
+	private void setupViews(JLabel lblWord) {
+		Object[] keys = baseSlangs.keySet().toArray(); 
+		setupResultList(keys, false);
 		
 		int randomIndex = new Random().nextInt(keys.length);
 		Slang randomSlang = baseSlangs.get(keys[randomIndex]);
 		lblWord.setText(randomSlang.getSlag() + " means " + randomSlang.getMeaning() + ".");
 	}
 	
-	private Map<String, Slang> readData(String filePath) {
+	private void setupResultList(Object[] keys, boolean isFromSearch) {
+		btnReset.setEnabled(false);
+		listMode.removeAllElements();
+		
+		for (int i = 0; i < keys.length; i++) {
+			Slang slang = baseSlangs.get(keys[i]);
+			if (slang != null) {
+				listMode.add(i, slang.toString());
+			} else if (isFromSearch) {
+				searchForMeaning(keys[i].toString());
+			}
+		}
+		btnReset.setEnabled(true);
+		/**
+		 * Thread resetThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("resetThread slang size=" + baseSlangs.size() + ", keys.length=" + keys.length);
+				
+			} 
+			
+		});
+		resetThread.start();
+		 * */
+	}
+	
+	private Map<String, Slang> readSlangsData(String filePath) {
 		Map<String, Slang> slangs = new HashMap<String, Slang>();
 		try {
 			File f = new File(filePath);
@@ -141,7 +246,7 @@ public class HomeScreen {
 							slang.setSlag(meaning[0]);
 							slang.setMeaning(meaning[1]);
 							slangs.put(meaning[0], slang);
-						
+							
 						}
 					}
 				}
@@ -154,5 +259,42 @@ public class HomeScreen {
 			e.printStackTrace();
 		}
 		return slangs;
+	}
+	
+	private String readHistoriesData() {
+		String result = "";
+		try {
+			File f = new File(HISTORIES_FILE_PATH);
+			if(f.exists()) { 
+				InputStream bin = new FileInputStream(HISTORIES_FILE_PATH);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(bin, "utf8"));
+				int lineIndex = 0;
+				while (reader.ready()) {
+					String line = reader.readLine();
+					if (!line.isEmpty()) result = line;
+				}
+				
+				reader.close();
+				bin.close();
+			}
+			
+		} catch(Exception e ) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	private void writeData(String filePath, String line, boolean isAppend) {
+		try {
+			//DataOutputStream ft = new DataOutputStream(new FileOutputStream(FILE_PATH));
+			FileWriter fw = new FileWriter(filePath, isAppend);
+			
+			fw.write(line);
+			fw.write("\n");
+			
+			fw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
